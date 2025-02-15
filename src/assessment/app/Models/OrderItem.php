bddfe5fb907fa1b7b8f9251de762d52ec6b19dbf
Allan Gallop <allangallop88@gmail.com>
2025-02-15 16:03:16 +0000

    Closes #23 - Create order models

---
+++ src/assessment/app/Models/OrderItem.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OrderItem extends Model
+{
+    protected $table = 'order_items';
+
+    protected $fillable = ['order_id', 'product_id', 'quantity', 'unit_price', 'sub_total_price'];
+
+        /**
+     * Get the user.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the product of the cart item.
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
